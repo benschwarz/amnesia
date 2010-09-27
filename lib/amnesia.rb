dd107b2201d1c3ac0c33c12d462209fefad04d28
Ben Schwarz <ben.schwarz@gmail.com>
2010-09-28 01:20:50 +1000

    Number to human size.

--- lib/amnesia.rb
+++ lib/amnesia.rb
@@ -1,4 +1,3 @@
-require 'bundler/setup'
 require 'sinatra'
 require 'memcache'
 require 'gchart'
@@ -27,6 +26,20 @@ module Amnesia
       def graph_url(data = [], labels = [])
         GChart.pie(data: data, size: '115x115').to_url
       end
+      
+      def number_to_human_size(size, precision=1)
+         size = Kernel.Float(size)
+         case
+           when size.to_i == 1;    "1 Byte"
+           when size < 1.kilobyte; "%d Bytes" % size
+           when size < 1.megabyte; "%.#{precision}f KB"  % (size / 1.0.kilobyte)
+           when size < 1.gigabyte; "%.#{precision}f MB"  % (size / 1.0.megabyte)
+           when size < 1.terabyte; "%.#{precision}f GB"  % (size / 1.0.gigabyte)
+           else                    "%.#{precision}f TB"  % (size / 1.0.terabyte)
+         end.sub(/([0-9])\.?0+ /, '\1 ' )
+       rescue
+         nil
+       end
     end
     
     get '/amnesia' do
