ffc2d00b3baa8016254f643be292183e68ab666a
glaszig <glaszig@gmail.com>
2019-12-25 06:01:18 +0100

    fixed size formatter

--- lib/amnesia.rb
+++ lib/amnesia.rb
@@ -9,6 +9,8 @@ require 'core_ext/array'
 
 module Amnesia
   class Application < Sinatra::Base
+    SIZE_UNITS = %w[ Bytes KB MB GB TB PB EB ]
+
     set :public_folder, File.join(File.dirname(__FILE__), 'amnesia', 'public')
     set :views, File.join(File.dirname(__FILE__), 'amnesia', 'views')
 
@@ -31,19 +33,20 @@ module Amnesia
         Gchart.pie(:data => data, :size => '115x115')
       end
 
-      def number_to_human_size(size, precision=1)
-         size = Kernel.Float(size)
-         case
-           when size.to_i == 1;    "1 Byte"
-           when size < 1.kilobyte; "%d Bytes" % size
-           when size < 1.megabyte; "%.#{precision}f KB"  % (size / 1.0.kilobyte)
-           when size < 1.gigabyte; "%.#{precision}f MB"  % (size / 1.0.megabyte)
-           when size < 1.terabyte; "%.#{precision}f GB"  % (size / 1.0.gigabyte)
-           else                    "%.#{precision}f TB"  % (size / 1.0.terabyte)
-         end.sub(/([0-9])\.?0+ /, '\1 ' )
-       rescue
-         nil
-       end
+      # https://github.com/rails/rails/blob/fbe335cfe09bf0949edfdf0c4b251f4d081bd5d7/activesupport/lib/active_support/number_helper/number_to_human_size_converter.rb
+      def number_to_human_size(number, precision=1)
+        number, base = Float(number), 1024
+
+        if number.to_i < 1024
+          "%d %s" % [ number.to_i, SIZE_UNITS.first ]
+        else
+          max = SIZE_UNITS.size - 1
+          exp = (Math.log(number) / Math.log(base)).to_i
+          exp = max if exp > max # avoid overflow for the highest unit
+          result = number / (1024**exp)
+          "%.#{precision}f %s" % [ result, SIZE_UNITS[exp] ]
+        end
+      end
     end
 
     get '/' do
