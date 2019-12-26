151370477e52f5e644e05f6aee963ca4acd84629
glaszig <glaszig@gmail.com>
2019-12-27 00:38:09 +0100

    use var consistently in helper

--- lib/amnesia.rb
+++ lib/amnesia.rb
@@ -37,13 +37,13 @@ module Amnesia
       def number_to_human_size(number, precision=1)
         number, base = Float(number), 1024
 
-        if number.to_i < 1024
+        if number.to_i < base
           "%d %s" % [ number.to_i, SIZE_UNITS.first ]
         else
           max = SIZE_UNITS.size - 1
           exp = (Math.log(number) / Math.log(base)).to_i
           exp = max if exp > max # avoid overflow for the highest unit
-          result = number / (1024**exp)
+          result = number / (base**exp)
           "%.#{precision}f %s" % [ result, SIZE_UNITS[exp] ]
         end
       end
